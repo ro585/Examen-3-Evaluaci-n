7623360f83dc34da74feeb3cc2e5a1c53e196212
HP196\CES <informatica@escuelaces.com>
2022-06-01 11:50:27 +0200

    COMMIT INICIAL COMMIT CABECERA MAIN COMMIT JAVADOC COMMIT rama CLASE COMMIT master FINAL

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -14,6 +14,25 @@ public class Main {
     
      public static void main(String[] args) {
     
+         ListaProductos Lp = new ListaProductos();
+         Producto Pro = new Producto();
+         
+         Producto Pro1 = new Producto("000100055","Tipo lacteo", 6, 6);
+         Producto Pro2 = new Producto("000116151","Tipo salsa", 2, 0.957);
+         Producto Pro3 = new Producto("008485401","Tipo carne", 12, 1);
+         Producto Pro4 = new Producto("478520001","Tipo fruta", 9, 1.8);
+         
+         Lp.addProducto(Pro1);
+         Lp.addProducto(Pro2);
+         Lp.addProducto(Pro3);
+         Lp.addProducto(Pro4);  
+         
+         Pro1.imprime_etiqueta();
+         Pro2.imprime_etiqueta();
+         Pro3.imprime_etiqueta();
+         Pro4.imprime_etiqueta();
+         
+         
          System.out.println("1º Gdam: Entornos de desarrollo");
-    }
+    }        
 }
